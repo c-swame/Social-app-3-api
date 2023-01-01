0b214e337e6c96cd075f7dc851db0fd7ecfa2e02
JoseAlguem <caio_swame@hotmail.com>
2022-12-31 23:18:50 -0300

    chore[Data AppDbContext]: Create our DB context

---
+++ Social-app-3-api.App/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Social_app_3_api.Model;
+using Social_app_3_api.Model.User;
+using Social_app_3_api.Model.Post;
+
+namespace Social_app_3_api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}

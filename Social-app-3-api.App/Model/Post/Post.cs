594500aabac92d095648d7df335abee2eedcc48c
JoseAlguem <caio_swame@hotmail.com>
2022-12-31 23:18:06 -0300

    git commit -m "chore[Model]: creates our base database models"

---
+++ Social-app-3-api.App/Model/Post/Post.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Social_app_3_api.Model.Post
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+
+        [Required, MinLength(1)]
+        [Column(TypeName = "varchar(500)")]
+        public string? Content { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public User.User? User{ get; set; }
+    }
+}
---
+++ Social-app-3-api.App/Model/User/User.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using ServiceStack.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace Social_app_3_api.Model.User
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MinLength(2)]
+        [Column(TypeName = "varchar(100)")]
+        public string? FirstName { get; set; }
+
+        [Required, MinLength(2)]
+        [Column(TypeName = "varchar(100)")]
+        public string? LastName { get; set; }
+
+        [Required, MinLength(2), Unique]
+        [Column(TypeName = "varchar(100)")]
+        [Index]
+        public string? UserName { get; set; }
+
+        // !!!!! UNIQUE NÃO ESTÁ FUNCIONANDO !!!!!
+        [Required, Unique]
+        [Index]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public UserRoleEnum Role { get; set; } = UserRoleEnum.RegularUser;
+
+        [Required, MinLength(8)]
+        [Column(TypeName = "varchar(100)")]
+        public string? Password { get; set; }
+
+        public ICollection<Post.Post> Posts { get; set; } = default!;
+    }
+}

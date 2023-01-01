30eabcaf920216748035396c08bd1e0afc85e966
JoseAlguem <caio_swame@hotmail.com>
2022-12-31 23:25:08 -0300

    chore[Repositories Interfaces]: create a interface for each one of ours Table Entities (Users and Posts)

---
+++ Social-app-3-api.App/Repositories/IPostRepository.cs
@@ -0,0 +1,16 @@
+﻿using Social_app_3_api.Model.Post;
+
+namespace Social_app_3_api.Repositories
+{
+    public interface IPostRepository
+    {
+        Task<List<Post>?> GetPosts();
+        Task<Post?> GetPost(int postId);
+        Task<List<Post>?> GetPostsByUser(int id);
+        Task<Post?> PostNewPost(Post postData);
+        //Task<Post?> PutPost(Post postData);
+        //Task<Post?> PatchPost(Post postData);
+        Task<Post?> UpdatePost(Post postData);
+        Task<Post?> DeletPost(Post post);
+    }
+}
---
+++ Social-app-3-api.App/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Social_app_3_api.Model.Post;
+using Social_app_3_api.Model.User;
+
+namespace Social_app_3_api.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<List<User>?> GetUsers();
+        Task<User?> GetUser(int userId);
+        Task<User?> GetUser(string email);
+        Task<User?> PostNewUser(User userData);
+        //Task<User?> PutUser(User userData);
+        //Task<User?> PatchUser(User userData);
+        Task<User?> UpdateUser(User userData);
+        Task<User?> DeletUser(User user);
+    }
+}

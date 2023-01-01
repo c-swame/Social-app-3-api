a9ebb5c3ad79828c3d18324e037bb76dd70a7e22
JoseAlguem <caio_swame@hotmail.com>
2022-12-31 23:29:10 -0300

    chore[Auth, Helpers]: creates some auth global constants and helpers to help creating password hashes

---
+++ Social-app-3-api.App/Constants/TokenConstants.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Social_app_3_api.Constants
+{
+    public class TokenConstants
+    {
+        public readonly static string Secret = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json").Build().GetValue<string>("JWTSecret");
+    }
+}
---
+++ Social-app-3-api.App/Helpers/AuthHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Social_app_3_api.Model.User;
+
+namespace Social_app_3_api.Helpers
+{
+    public static class AuthHelper
+    {
+        public static string GenerateHash(User user, string password)
+        {
+            PasswordHasher<User> passwordHasher= new();
+
+            string hashedPassword = passwordHasher.HashPassword(user, password);
+
+            return hashedPassword;
+        }
+        public static bool VerifyHash(User user, string password, string hashedPassword)
+        {
+            PasswordHasher<User> passwordHasher = new();
+
+            PasswordVerificationResult isValid = passwordHasher.VerifyHashedPassword(user, hashedPassword, password);
+            // ajustar método para possibilidar a criação de novo hash quando necessário.
+            if(isValid.ToString() == "Failed")
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}

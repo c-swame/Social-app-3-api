5530ef7df758c58eda4aceea76bfa013cbe6837b
JoseAlguem <caio_swame@hotmail.com>
2023-01-02 12:22:33 -0300

    doc[Respositories PostRepository]: document some methods

--- Social-app-3-api.App/Repositories/PostRepository.cs
+++ Social-app-3-api.App/Repositories/PostRepository.cs
@@ -6,6 +6,10 @@ using Social_app_3_api.Model.User;
 
 namespace Social_app_3_api.Repositories
 {
+    /// <summary>
+    /// Classe para fazer intermédio e aplicar regras de negócio entre o banco de dados (model) e os controllers
+    /// model base: Post
+    /// </summary>
     public class PostRepository : IPostRepository
     {
         private readonly AppDbContext _context;
@@ -13,6 +17,10 @@ namespace Social_app_3_api.Repositories
         {
             _context = context;
         }
+        /// <summary>
+        /// método responsável por buscar todos os posts
+        /// </summary>
+        /// <returns>Lista com todos os posts (apenas o id do usuário responsável é enviado nessa requisição)</returns>
         public async Task<List<Post>?> GetPosts()
         {
             try
@@ -28,10 +36,17 @@ namespace Social_app_3_api.Repositories
             }
         }
 
+        /// <summary>
+        /// método responsável por buscar posts através do id do post
+        /// </summary>
+        /// <param name="postId"></param>
+        /// <returns>objeto do tipo Post incluindo o ojeto que representa o usuário dono do post</returns>
         public async Task<Post?> GetPost(int postId)
         {
             try
             {
+                //!!!!! Remover senha do usuário retorno. -> criar classe que contenha apenas os dados do usuário que devem ser retornados (superclasse? derivada?)
+                //!!!!!? Remover o usuário do retorno? !+
                 return await _context.Posts
                     .Include(c => c.User)
                     .FirstAsync(c => c.PostId == postId);
@@ -43,6 +58,11 @@ namespace Social_app_3_api.Repositories
             }
         }
 
+        /// <summary>
+        /// Retorna todos os posts de um único usuário
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Retorna os posts do usuário dono do id passado como argumento</returns>
         public async Task<List<Post>?> GetPostsByUser(int id)
         {
             try
@@ -59,6 +79,14 @@ namespace Social_app_3_api.Repositories
             }
         }
 
+        /// <summary>
+        /// Cria um novo post. É esperado que todos os dados do novo post, incluindo o id do usuário sejam passados // ?????!!!!! Tornar o campo Usuário um campo virtual? perco performace nesse caso usando um capo virtual?????
+        /// </summary>
+        /// <param name="postData"></param>
+        /// <returns>
+        /// sucesso: Representação do objeto criado
+        /// falha: null
+        /// </returns>
         public async Task<Post?> PostNewPost(Post postData)
         {
             try
@@ -76,6 +104,14 @@ namespace Social_app_3_api.Repositories
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="postData"></param>
+        /// <returns>
+        /// sucesso: Representação do objeto atualizado
+        /// falha: nullo
+        /// </returns>
         public async Task<Post?> UpdatePost(Post postData)
         {
             try
@@ -104,6 +140,14 @@ namespace Social_app_3_api.Repositories
         //    throw new NotImplementedException();
         //}
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="post"></param>
+        /// <returns>
+        /// sucesso: Representação do elemento deletado;
+        /// falha: null
+        /// </returns>
         public async Task<Post?> DeletPost(Post post)
         {
             try {
